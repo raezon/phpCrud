38f016b9fad01844c519f354e604953eb707db9f
ammar <amardjebabla10@gmail.com>
2024-03-18 09:27:30 +0100

    Saving the composer.json

---
+++ migrations/20240318081309_create_table_product.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateTableProduct extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+
+        $table = $this->table('product', ['id' => false, 'primary_key' => 'id']);
+        $table->addColumn('id', 'biginteger', ['identity' => true])
+              ->addColumn('nom', 'string', ['limit' => 255])
+              ->addColumn('mark', 'string', ['limit' => 255])
+              ->addColumn('stars', 'integer')
+              ->addColumn('prix', 'decimal', ['precision' => 10, 'scale' => 2])
+              ->addColumn('description', 'string', ['limit' => 255])
+              ->addColumn('actif', 'boolean')
+              ->create();
+    }
+}
